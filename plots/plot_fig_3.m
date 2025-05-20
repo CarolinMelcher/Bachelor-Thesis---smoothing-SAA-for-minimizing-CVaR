288d10fa6beef572b6253e59908e2c2227e2a4a8
CarolinMelcher <caro.melcher@web.de>
2025-05-20 09:20:34 +0200

    Update plot_fig_3.m

--- plots/plot_fig_3.m
+++ plots/plot_fig_3.m
@@ -3,17 +3,13 @@
 n_evaluations = 250;                               % Number of evaluations
 n = round(logspace(2, 7, n_evaluations));          % Generate 50 sample sizes logarithmically spaced between 10^2 and 10^7
 
-% Preallocation of arrays for the results
-t = zeros(length(n), 1);              % Execution times
-iterations = zeros(length(n), 1);     % Iteration counts
+% Preallocation of array for the results
 c = zeros(length(n), 1);              % CVaR values
 
 % Loop over each sample size to compute CVaR, execution time, and iterations
 for i = 1:n_evaluations
-    [~, cvar, time, iter] = cvar_optimized(0.99, 10^-4, n(i));  % Call optimizer at 99% confidence, ε=10^-4, N=n(i)
-    c(i)   = cvar;   
-    t(i)  = time;    
-    iterations(i) = iter;      
+    [~, cvar, ~, ~] = cvar_optimized(0.99, 10^-4, n(i));  % Call optimizer at 99% confidence, ε=10^-4, N=n(i)
+    c(i)   = cvar;         
 end
 
 % Plot 
